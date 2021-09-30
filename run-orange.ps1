b049838261e01c7860bdbd74167a44b7fc1babeb
Denis Rotanov <mapholameth@gmail.com>
2021-10-01 02:41:56 +1000

    Fix run scripts

--- run-orange.ps1
+++ run-orange.ps1
@@ -2,4 +2,4 @@ $CurrentPath = Get-Location
 $DotnetPath = Join-Path -Path $CurrentPath -ChildPath "dotnet"
 $Env:DOTNET_ROOT = $DotnetPath
 $Env:Path += $DotnetPath
-Start-Process -FilePath ".\Citrus\Orange\Launcher\bin\Win\Release\Launcher.exe"
+Invoke-Expression "&'.\Citrus\Orange\Launcher\bin\Win\Release\Launcher.exe'"
