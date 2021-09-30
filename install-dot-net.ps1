9d694e1c30ab70cf75bd172f0a2b0d3f19617668
Denis Rotanov <mapholameth@gmail.com>
2021-09-30 21:27:05 +1000

    Dont make dir if one already there in install script

--- install-dot-net.ps1
+++ install-dot-net.ps1
@@ -3,8 +3,10 @@ $Arch = "x64"
 $SDKVersion = "5.0.400"
 $RuntimeVersion = "5.0.2"
 $Runtime = "dotnet"
-
-New-Item -ItemType "directory" -Path $Path
+If(!(test-path $Path))
+{
+    New-Item -ItemType "directory" -Path $Path
+}
 Invoke-WebRequest -UseBasicParsing https://dot.net/v1/dotnet-install.ps1 -Outfile $Path/dotnet-install.ps1
 Invoke-Expression "&'$Path/dotnet-install.ps1' -Architecture $Arch -Version $SDKVersion -InstallDir $Path"
 Invoke-Expression "&'$Path/dotnet-install.ps1' -Architecture $Arch -Runtime $Runtime -Version $RuntimeVersion -InstallDir $Path"
