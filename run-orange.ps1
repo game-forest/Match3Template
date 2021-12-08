5d3b7f397c0c6a6ae42b6c4c2c7b38943d85ade7
Denis Rotanov <mapholameth@gmail.com>
2021-12-09 04:29:57 +1000

    Update Citrus

--- run-orange.ps1
+++ run-orange.ps1
@@ -1,5 +1,1 @@
-$CurrentPath = Get-Location
-$DotnetPath = Join-Path -Path $CurrentPath -ChildPath "dotnet"
-$Env:DOTNET_ROOT = $DotnetPath
-$Env:Path += $DotnetPath
-Invoke-Expression "&'.\Citrus\Orange\Launcher\bin\Win\Release\Launcher.exe'"
+Citrus\Run.ps1 -RunProject "Orange"
--- run-tangerine.ps1
+++ run-tangerine.ps1
@@ -1,5 +1,1 @@
-$CurrentPath = Get-Location
-$DotnetPath = Join-Path -Path $CurrentPath -ChildPath "dotnet"
-$Env:DOTNET_ROOT = $DotnetPath
-$Env:Path += ';' + $DotnetPath
-Invoke-Expression "&'.\Citrus\Orange\Launcher\bin\Win\Release\Launcher.exe' -b Tangerine\Tangerine.Win.sln -r Tangerine\Tangerine\bin\Release\Tangerine.exe"
+Citrus\Run.ps1 -RunProject "Tangerine"
